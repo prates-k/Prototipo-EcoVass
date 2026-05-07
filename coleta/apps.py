1000fa40f5a3851ff23eab23074fe1281bc05e53
prates-k <eng.kauaprates@gmail.com>
2026-05-07 17:56:56 -0300

    feat: implementa automação de EcoPontos via Django Signals

--- coleta/apps.py
+++ coleta/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class ColetaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'coleta'
+
+    def ready(self):
+        import coleta.signals
---
+++ coleta/signals.py
@@ -0,0 +1,31 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import SolicitacaoColeta
+from gamificacao.models import Carteira, TransacaoPonto
+
+@receiver(post_save, sender=SolicitacaoColeta)
+def creditar_pontos_coleta(sender, instance, created, **kwargs):
+    '''
+    Sinal que dispara sempre que uma SolicitacaoColeta é salva
+    '''
+    
+    if instance.status == 'CONCLUIDO':
+        carteira, _ = Carteira.objects.get_or_create(usuario=instance.usuario)
+        
+        descricao_venda = f'Coleta #{instance.id} de {instance.tipo_residuo} concluída'
+        ja_creditado = TransacaoPonto.objects.filter(descricao=descricao_venda).exists()
+        
+    if not ja_creditado:
+        valor_pontos = 100
+        
+        carteira.saldo += valor_pontos
+        carteira.total_historico += valor_pontos
+        carteira.save()
+        
+        TransacaoPonto.objects.create(
+            carteira=carteira,
+            quantidade=valor_pontos,
+            tipo='CREDITO',
+            descricao=descricao_venda
+        )
+        print(f'Sucesso: 100 pontos creditados para {instance.usuario.username}')
